5fb14999f6b42a5b84f780ff253845406b03e103
jnzhang622 <jnzhang622@gmail.com>
2020-03-19 23:20:37 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costume_stores do |c|
+            c.string :name
+            c.string :location
+            c.integer :costume_inventory
+            c.integer :employees
+            c.boolean :still_in_business
+            c.datetime :opening
+            c.datetime :closing
+            c.timestamp :created_at
+            c.timestamp :updated_at
+        end
+    end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |h|
+            h.string :name
+            h.string :location
+            h.string :theme
+            h.float :price
+            h.boolean :family_friendly
+            h.datetime :opening
+            h.datetime :closing
+            h.string :description
+            c.timestamp :created_at
+            c.timestamp :updated_at
+        end
+    end
+end
